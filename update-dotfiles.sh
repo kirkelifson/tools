998b258239c741537350c3029a0da45acdba3a4d
Kirk Elifson <kirk@parodybit.net>
2015-01-10 17:56:39 -0500

    exec the install script

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,7 +1,7 @@
 echo "\n[+] Connecting to andromeda..."
-ssh -t andromeda "cd dot-files; git pull; sudo -- sh -c 'cd ~/dot-files; git pull'"
+ssh -t andromeda "cd dot-files; git pull; ./install; sudo -- sh -c 'cd ~/dot-files; git pull; ./install'"
 echo "\n[+] Connecting to apollo..."
-ssh -t apollo "cd dot-files; git pull; sudo -- sh -c 'cd ~/dot-files; git pull'"
+ssh -t apollo "cd dot-files; git pull; ./install; sudo -- sh -c 'cd ~/dot-files; git pull; ./install'"
 echo "\n[+] Connecting to pythagoras..."
-ssh -t pythagoras "cd dot-files; git pull; sudo -- sh -c 'cd ~/dot-files; git pull'"
+ssh -t pythagoras "cd dot-files; git pull; ./install; sudo -- sh -c 'cd ~/dot-files; git pull; ./install'"
 
