822b109102320d4948a5068d19352d8186747214
Kirk Elifson <kirk@parodybit.net>
2015-01-16 12:52:16 -0500

    Adjust for rename of install script

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,7 +1,7 @@
 echo "\n[+] Connecting to andromeda..."
-ssh -t andromeda "cd dot-files; git pull; ./install; sudo -- sh -c 'cd ~/dot-files; git pull; ./install'"
+ssh -t andromeda "cd dot-files; git pull; ./install.sh; sudo -- sh -c 'cd ~/dot-files; git pull; ./install.sh'"
 echo "\n[+] Connecting to apollo..."
-ssh -t apollo "cd dot-files; git pull; ./install; sudo -- sh -c 'cd ~/dot-files; git pull; ./install'"
+ssh -t apollo "cd dot-files; git pull; ./install.sh; sudo -- sh -c 'cd ~/dot-files; git pull; ./install.sh'"
 echo "\n[+] Connecting to pythagoras..."
-ssh -t pythagoras "cd dot-files; git pull; ./install; sudo -- sh -c 'cd ~/dot-files; git pull; ./install'"
+ssh -t pythagoras "cd dot-files; git pull; ./install.sh; sudo -- sh -c 'cd ~/dot-files; git pull; ./install.sh'"
 
