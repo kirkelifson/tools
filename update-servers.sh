ea1bddd82be3c28460afb32c7f9dd488f3804734
Kirk Elifson <kirk@parodybit.net>
2015-03-13 15:14:18 -0400

    incorrect array syntax

--- update-servers.sh
+++ update-servers.sh
@@ -1,5 +1,5 @@
 servers=(andromeda apollo pythagoras euclid)
-for server in $servers
+for server in ${servers[@]}
 do
     echo "\n[+] Connecting to $server..."
     ssh -o ConnectTimeout=5 -t $server "./update.sh"
